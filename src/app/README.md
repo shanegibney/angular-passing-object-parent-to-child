3008ad9fee1a7f21cb84fb9afce624d93caafa0b
shanegibney <shanegibney@gmail.com>
2017-07-24 17:59:04 +0100

    first commit

---
+++ src/app/README.md
@@ -0,0 +1,1 @@
+# angular-passing-object-parent-to-child
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit, OnChanges {
+  @Input() data: {name: string, value: number}[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(){
+
+  }
+}

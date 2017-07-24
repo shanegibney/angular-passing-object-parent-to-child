734fd786cad92b0e918da48998aebbfcf8ee4e96
shanegibney <shanegibney@gmail.com>
2017-07-24 20:41:09 +0100

    corrected error in child template

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 })
 export class ChildComponent implements OnInit, OnChanges {
   @Input() data: {name: string, value: number}[] = [];
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   ngOnChanges(){
+    let info: {name: string, value: number}[]=[];
     console.log("changed");
+    // console.log(this.data[0].value);
+    for(let i=0; i<this.data.length; i++){
+      info[i] = this.data[i];
+    }
+    for(let i=0; i<info.length; i++){
+      console.log("name: ", info[i].name, "value: ", info[i].value);
+    }
   }
 }
